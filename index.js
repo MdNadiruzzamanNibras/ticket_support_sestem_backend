6ac01437483f03c0ec51045c8c86a0333803d58e
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-12-02 11:40:15 +0600

    db pass

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = process.env.DB_URI
+const uri = `mongodb+srv://ticket:${process.env.DB_PASS}@cluster0.nvfsjha.mongodb.net/?retryWrites=true&w=majority`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
